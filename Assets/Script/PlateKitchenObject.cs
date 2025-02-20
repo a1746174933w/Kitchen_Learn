73257211d62997cd8e65a679aa9ff50cedd38969
何森 <1746174933@qq.com>
2025-02-20 08:53:27 +0800

    第四次提交

--- Assets/Script/PlateKitchenObject.cs
+++ Assets/Script/PlateKitchenObject.cs
@@ -41,4 +41,8 @@ public class PlateKitchenObject : KitchenObject{
         }
        
     }
+
+    public List<KitchenObjectSO> GetKitchenObjectSOList() {
+        return kitchenObjectSOList;
+    }
 }
